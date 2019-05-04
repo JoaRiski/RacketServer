c5fcb9c438d40f0d9d769b3e1549b4cfa2d8e0b3
Joonatan Bergholm <joonardob@gmail.com>
2019-05-04 22:42:00 +0300

    added keys and origo parameter

--- racket/generate_model.py
+++ racket/generate_model.py
@@ -1,6 +1,7 @@
 import numpy as np
 import json
 
+
 def getdata(idx, key='Right_Wrist', origo='Right_Shoulder'):
     data = json.load(open(f'data/data-{idx}.json', 'r'))
     data = sorted(data, key=lambda f: f['time'])
@@ -9,21 +10,30 @@ def getdata(idx, key='Right_Wrist', origo='Right_Shoulder'):
     t = [f['time'] - data[0]['time'] for f in data]
     return (x, y, t)
 
+
 def make_interpolation_by_L2(x, y, t):
     n = len(x)
     assert n == len(y)
     L = [0]
-    for i in range(n-1):
-        L.append(np.sqrt((x[i+1]-x[i])**2 + (y[i+1] - y[i])**2) + L[-1])
+    for i in range(n - 1):
+        L.append(
+            np.sqrt((x[i + 1] - x[i]) ** 2 + (y[i + 1] - y[i]) ** 2) + L[-1]
+        )
     L = np.array(L)
 
     def interpolation(p):
         assert 0 <= p <= 1
-        idx = np.where(L < p*L[-1])[0][-1]
-        coeff = p - L[idx]/L[-1]
+        idx = np.where(L < p * L[-1])[0][-1]
+        coeff = p - L[idx] / L[-1]
         return np.array(
-            [(x[idx+1] - x[idx])*coeff + x[idx], (y[idx+1] - y[idx])*coeff + y[idx]])
+            [
+                (x[idx + 1] - x[idx]) * coeff + x[idx],
+                (y[idx + 1] - y[idx]) * coeff + y[idx],
+            ]
+        )
+
     return interpolation
+
 
 def make_interpolation_by_T(x, y, t):
     n = len(x)
@@ -31,17 +41,28 @@ def make_interpolation_by_T(x, y, t):
 
     def interpolation(p):
         assert 0 <= p <= 1
-        idx = np.where(t < p*t[-1])[0][-1]
-        coeff = p - t[idx]/L[-1]
+        idx = np.where(t < p * t[-1])[0][-1]
+        coeff = p - t[idx] / L[-1]
         return np.array(
-            [(x[idx+1] - x[idx])*coeff + x[idx], (y[idx+1] - y[idx])*coeff + y[idx]])
+            [
+                (x[idx + 1] - x[idx]) * coeff + x[idx],
+                (y[idx + 1] - y[idx]) * coeff + y[idx],
+            ]
+        )
+
     return interpolation
 
-def make_final_model():
-    interps = []
-    for i in range(1, 18):
-        x_, y_, t_ = getdata(i, key='Right_Wrist')
-        #plt.plot(x_, y_, linestyle='dashed', linewidth=1)
-        interps.append(make_interpolation_by_L2(x_, y_, t_))
 
-    return lambda p: sum(interp(p) for interp in interps) / len(interps)
+def make_final_models(keys):
+    models = {}
+    for key in keys:
+        interps = []
+        for i in range(1, 18):
+            x_, y_, t_ = getdata(i, key='Right_Wrist')
+            # plt.plot(x_, y_, linestyle='dashed', linewidth=1)
+            interps.append(make_interpolation_by_L2(x_, y_, t_))
+
+        models[key] = lambda p: sum(interp(p) for interp in interps) / len(
+            interps
+        )
+    return models
