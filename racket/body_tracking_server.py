48efcdb848bf294a87003daf1df0921f0169d8e9
Joonatan Bergholm <joonardob@gmail.com>
2019-05-05 02:45:33 +0300

    draw

--- racket/body_tracking_server.py
+++ racket/body_tracking_server.py
@@ -2,10 +2,19 @@ import asyncio
 import json
 from . import generate_model as models
 import numpy as np
+import pygame
 
 ORIGO = 'Right_Shoulder'
 SCALE = 'Right_Elbow'
 KEYS = ['Right_Elbow', 'Right_Wrist']
+
+pygame.init()
+screen = pygame.display.set_mode((500, 500))
+screen.fill((255, 255, 255))
+s = pygame.Surface(screen.get_size(), pygame.SRCALPHA, 32)
+
+screen.blit(s, (0, 0))
+pygame.display.flip()
 
 
 def to_pos(json):
@@ -40,6 +49,12 @@ class BodyTrackingProtocol:
         for key in KEYS:
             pos = (to_pos(frame[key]) - to_pos(frame[ORIGO])) / scale
             points[key] = pos
+            pygame.gfxdraw.aacircle(
+                s, 250 + pos[0] * 200, 250 + pos[1] * 200, 10, (10, 10, 10)
+            )
+
+        screen.blit(s, (0, 0))
+        pygame.display.flip()
 
         data = self._follower.test(points)
         await self._feedback_server.messages.put(data)
@@ -47,6 +62,7 @@ class BodyTrackingProtocol:
     @staticmethod
     async def main(loop, feedback_server):
         import os
+
         transport, _ = await loop.create_datagram_endpoint(
             lambda: BodyTrackingProtocol(feedback_server),
             local_addr=(os.environ["UDP_IP"], 8888),
